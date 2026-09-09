364d457e25bf6d81922096c72c41e0083aeb8ece
sujan <sujankapali50@gmail.com>
2026-09-09 07:13:50 +0200

    Updated init.py

--- src/paper_explorer/__init__.py
+++ src/paper_explorer/__init__.py
@@ -1,18 +1,21 @@
-from paper_explorer.crawler.arxiv_crawler import ArxivCrawler
+from paper_explorer.crawler.arxiv_client import ArxivAPIError, ArxivClient
 from paper_explorer.data.models import Paper
 from paper_explorer.data.storage import PaperStore
-from paper_explorer.embeddings.embedder import Embedder
-from paper_explorer.search.engine import SearchEngine
+from paper_explorer.embeddings.embedding_model import EmbeddingModel
 from paper_explorer.search.index import VectorIndex
+from paper_explorer.search.results import SearchResult
+from paper_explorer.search.searcher import PaperSearcher
 from paper_explorer.summarize.topic_summary import Topic, TopicSummarizer
 
 __all__ = [
     "Paper",
     "PaperStore",
-    "ArxivCrawler",
-    "Embedder",
+    "ArxivClient",
+    "ArxivAPIError",
+    "EmbeddingModel",
     "VectorIndex",
-    "SearchEngine",
+    "SearchResult",
+    "PaperSearcher",
     "Topic",
     "TopicSummarizer",
 ]
